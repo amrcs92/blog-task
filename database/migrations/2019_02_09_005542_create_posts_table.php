0b1bd6432d7f37017ccdca49ef3fb0f84fc63445
amrcs92 <amrcs1992@gmail.com>
2019-02-09 03:02:07 +0200

    created posts table

---
+++ database/migrations/2019_02_09_005542_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 200);
+            $table->text('description');
+            $table->unsignedInteger('category_id');
+            $table->timestamps();
+            $table->foreign('category_id')->refrences('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
